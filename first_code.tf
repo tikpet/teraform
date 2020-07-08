fcfd534eb4f66682f57dc8f1c65926c80c1be5a1
Tiko <tikpet@gmail.com>
2020-07-09 03:10:26 +0400

    autorisation problem

--- first_code.tf
+++ first_code.tf
@@ -1,23 +1,28 @@
-
-
-
 provider "aws" {
- profile = "default"
  region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu_server"{
+data "aws_ami" "ubuntu" {
  most_recent = true
- owners = ["099720109477"]
+
+ /*filter {
+  name   = "name"
+  values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+ }
 
  filter {
-  name = "name"
-  values = ["ubuntu"]
+  name   = "virtualization-type"
+  values = ["hvm"]
  }
+*/
+ owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "something" {
-
- ami = data.aws_ami.ubuntu_server.id
+resource "aws_instance" "web" {
+ ami           = "${data.aws_ami.ubuntu.id}"
  instance_type = "t2.micro"
+
+ tags = {
+  Name = "HelloWorld"
+ }
 }
